df52fc1e5ef92de61e9f44412d7b251766b3db11
aloe-99 <100123642+aloe-99@users.noreply.github.com>
2025-04-08 13:41:35 +0300

    add Schemas

---
+++ models/task.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  executor: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  indate: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  title: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('task', taskSchema);
